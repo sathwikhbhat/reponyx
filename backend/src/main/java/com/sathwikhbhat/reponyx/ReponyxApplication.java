e1a387dc41f771fd3a0468b86559d2a6f03258e8
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-08-24 16:28:42 +0530

    initialize project with Maven setup and Spring Boot configuration

---
+++ backend/src/main/java/com/sathwikhbhat/reponyx/ReponyxApplication.java
@@ -0,0 +1,13 @@
+package com.sathwikhbhat.reponyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReponyxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReponyxApplication.class, args);
+	}
+
+}
