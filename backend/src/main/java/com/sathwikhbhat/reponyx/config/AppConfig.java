e3af9f2a122fe3f94018f448d862e7ecd15a3905
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-08-31 16:04:21 +0530

    feat: add GithubApiClient for GitHub API integration

---
+++ backend/src/main/java/com/sathwikhbhat/reponyx/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.sathwikhbhat.reponyx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
+}
